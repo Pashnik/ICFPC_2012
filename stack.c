f1b0c52bbdfdba582dcc8c1369007f7c34bc5671
pashnik <pashanikon15@gmail.com>
2018-09-28 12:30:14 +0300

    A-STAR algorithm for local path

---
+++ stack.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "headers/stack.h"
+
+#define STACK_VAL 100 // stack length
+
+int sp = 0;
+char command[STACK_VAL]; // numbers stack
+
+void pushCommand(char value) {
+    if (sp < STACK_VAL) command[sp++] = value;
+    else exit(-1);
+}
+
+int popCommand(void) {
+    if (sp >= 0) return command[--sp];
+    else exit(-1);
+}
+
+int isEmpty(void) {
+    if (sp > 0) return 0;
+    else return 1;
+}
